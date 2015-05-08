004cd51f82fbaecc4a02a81c3cc1c3af6bf9b77a
ideawu <ideawu@me>
2015-05-08 16:14:24 +0800

    update

--- src/app.cpp
+++ src/app.cpp
@@ -34,12 +34,11 @@ int Application::main(int argc, char **argv){
 	parse_args(argc, argv);
 	init();
 
-	log_info("config : %s", this->app_args.conf_file.c_str());
-	log_info("pidfile: %s, pid: %d", app_args.pidfile.c_str(), (int)getpid());
-	log_info("logger");
-	log_info("    level : %s", Logger::shared()->level_name().c_str());
-	log_info("    output: %s", Logger::shared()->output_name().c_str());
-	log_info("    rotate: %" PRId64, Logger::shared()->rotate_size());
+	log_info("config    : %s", this->app_args.conf_file.c_str());
+	log_info("pidfile   : %s, pid: %d", app_args.pidfile.c_str(), (int)getpid());
+	log_info("log_level : %s", Logger::shared()->level_name().c_str());
+	log_info("log_output: %s", Logger::shared()->output_name().c_str());
+	log_info("log_rotate: %" PRId64, Logger::shared()->rotate_size());
 
 	write_pid();
 	run();
