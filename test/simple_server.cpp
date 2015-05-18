a00487d30a7a6ddeef5b4328f5bf9de2ef3503ff
ideawu <ideawu@me>
2015-05-18 19:09:45 +0800

    update README.md

---
+++ test/simple_server.cpp
@@ -0,0 +1,34 @@
+#include "sim/sim.h"
+
+class MyHandler : public sim::Handler
+{
+public:
+	virtual sim::HandlerState proc(const sim::Request &req, sim::Response *resp){
+		std::string cmd = req.msg.type();
+		if(cmd == "ping"){
+			resp->msg.add("ok");
+			resp->msg.add("pong");
+		}else{
+			resp->msg.add("ok");
+			resp->msg.add(cmd);
+		}
+		return this->resp();
+	}
+};
+
+int main(int argc, char **argv){
+	const char *ip = "127.0.0.1";
+	int port = 8800;
+	sim::Server *serv = sim::Server::listen(ip, port);
+	if(!serv){
+		log_fatal("%s", strerror(errno));
+		exit(0);
+	}
+	log_info("server listen on %s:%d", ip, port);
+	
+	MyHandler handler;
+	serv->add_handler(&handler);
+	
+	serv->loop();
+	return 0;
+}
