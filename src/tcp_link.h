45f7a25c28bd4e0644c8e3837331f4a36dd6ef7d
ideawu <ideawu@me>
2018-07-27 19:14:57 +0800

    update

--- src/tcp_link.h
+++ src/tcp_link.h
@@ -16,6 +16,7 @@ class Server
 {
 public:
 	Link* link() const;
+	void proc();
 };
 
 class Session
@@ -23,5 +24,70 @@ class Session
 public:
 	Link* link() const;
 	Parser* parser() const;
+	
 };
+
+// !!! 多线程时，必须用引用计数，否则无法判断何时释放内存
+// std::shared_ptr?
+
+// TODO:
+// 服务器循环
+while(1){
+	// 处理定时器事件
+	foreach(timers as timer){
+		timer.invoke();
+	}
+	
+	// 处理网络读事件
+	foreach(in_links as link){
+		lock(link){
+			if(link.net_read() == -1){
+				break_lock;
+			}
+			while(req = link.recv()){
+				req_queue.add(req);
+			}
+		}
+	}
+	
+	// 处理响应
+	while(link = out_links.pop()){
+		lock(link){
+			if(link.closed()){
+				break_lock;
+			}
+			link.flush();
+		}
+	}
+}
+
+async proc_recv(){
+	while(req = req_queue.pop()){
+		resp = serv.proc(req);
+		lock(link){
+			if(link.closed()){
+				break_lock;
+			}
+			link.send(resp);
+			out_links.add_once(link);
+		}
+	}
+}
+
+async business(){
+	while(event = pull_event()){
+		msg = generate_resp();
+		lock(link){
+			if(link.closed()){
+				break_lock;
+			}
+			link.send(msg);
+			out_links.add_once(link);
+		}
+	}
+}
+
+/*
+*/
+
 
